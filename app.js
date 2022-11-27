1a88e8f64fa2c3e9dd5e60260aac2796f0fb4a55
Djany Mouton <djany.developpement@gmail.com>
2022-11-27 13:31:28 +0100

    project restart

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // Selected Element 
-
 let body = document.querySelector("body");
-
-// Created Element
-
-let countdownContainer = document.createElement('div');
-
+let title = document.createElement("h1")
 
 // Body parameter
 
@@ -15,20 +10,6 @@ body.style.justifyContent = 'center';
 body.style.backgroundColor = 'Black';
 body.style.margin = '0';
 body.style.height = '100vh';
-
-// Countdown container style
-
-countdownContainer.style.width = '90vw';
-countdownContainer.style.height = '80vh';
-countdownContainer.style.border = '0.2px solid white';
-countdownContainer.style.borderRadius = '10px';
-countdownContainer.style.color = 'white';
-
-document.body.append(countdownContainer);
-
-// Create the title of the countdown container
-
-countdownContainer.innerHTML = "<h1>Halving day of bitcoin</h1>"
 
 
 
